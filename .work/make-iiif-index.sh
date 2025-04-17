d52ae1b86027729ce2a13cb126655f16bc2997bd
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2025-04-17 17:39:40 +0200

    add feedback

--- .work/make-iiif-index.sh
+++ .work/make-iiif-index.sh
@@ -5,3 +5,4 @@ cd ./iiif && (
   for f in *.json; do cat ../.work/template.tpl | sed -e "s/%manifest%/$f/g"; done >> $OUT
   cat ../.work/foot.html >> $OUT
 )
+echo "after a 'git commit' and 'git push', check https://brambg.github.io/static-file-server/iiif/"
